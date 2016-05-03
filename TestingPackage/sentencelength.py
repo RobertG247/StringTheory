988cec4dbdeca7f741a4b179cd08d2824489b43c
RobertG247 <rguzman.p247@gmail.com>
2016-05-03 17:35:37 -0400

    to learn better

---
+++ TestingPackage/sentencelength.py
@@ -0,0 +1,9 @@
+f = open("test.txt", "r")
+myList = []
+for line in f:
+    myList.append(line)
+b = len(myList)
+for a in range(0, b):
+    print(myList[a])
+
+    
---
+++ TestingPackage/words.py
@@ -0,0 +1,51 @@
+#def split_line(text):
+
+    # split the text
+    #words = text.split()
+
+    # for each word in the line:
+    #for word in words:
+
+        #print("the word")
+        #print(word)
+#def word
+    #word =
+#line = "a sentence with a few words"
+#line.split()
+#['a', 'sentence', 'with', 'a', 'few', 'words']
+#print ("index value of each word in line")
+#list.count(x)
+
+
+#def myString():
+    #myList = []
+    #myString="spaceEssay.txt"
+    #sentence=[]
+
+    #f = open("")
+
+    #import sys
+    #def main():
+        #def myString():
+            #myList = []
+
+       # myString = "test.txt"
+        #sentence = []
+
+        f = open("test.txt", "r")
+
+        for line in f:
+            myList.append(line)
+        b = len(myList)
+
+        for a in range(0, b):
+            sentence.append(myList[a].split())
+            print(sentence[a])
+
+
+
+
+
+
+
+
